5124710ab3f7bee93be13d42321fc56249f637ef
Rokeer <rockswing@gmail.com>
2012-12-01 16:48:16 +0800

    First Demo

---
+++ CarPlate_Demo_1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace CarPlate_Demo_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.imageBox5 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.imageBox6 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 91);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(361, 289);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(379, 91);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(361, 289);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Location = new System.Drawing.Point(746, 91);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(361, 289);
+            this.imageBox3.TabIndex = 4;
+            this.imageBox3.TabStop = false;
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.Location = new System.Drawing.Point(12, 428);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(361, 289);
+            this.imageBox4.TabIndex = 5;
+            this.imageBox4.TabStop = false;
+            // 
+            // imageBox5
+            // 
+            this.imageBox5.Location = new System.Drawing.Point(379, 428);
+            this.imageBox5.Name = "imageBox5";
+            this.imageBox5.Size = new System.Drawing.Size(361, 289);
+            this.imageBox5.TabIndex = 6;
+            this.imageBox5.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(520, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Original Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(507, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Gray Scale Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(856, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Equalize Histogram Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Smooth Image";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(506, 413);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Edge Detection";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(895, 413);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Final Image";
+            // 
+            // imageBox6
+            // 
+            this.imageBox6.Location = new System.Drawing.Point(744, 428);
+            this.imageBox6.Name = "imageBox6";
+            this.imageBox6.Size = new System.Drawing.Size(361, 289);
+            this.imageBox6.TabIndex = 7;
+            this.imageBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 729);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox6);
+            this.Controls.Add(this.imageBox5);
+            this.Controls.Add(this.imageBox4);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "License Plate Recognition Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private Emgu.CV.UI.ImageBox imageBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private Emgu.CV.UI.ImageBox imageBox6;
+    }
+}
+
---
+++ CarPlate_Demo_1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace CarPlate_Demo_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
+                Image<Gray, byte> gray_image = My_Image.Convert<Gray, byte>();
+                Image<Gray, byte> eh_gray_image = My_Image.Convert<Gray, byte>();
+                Image<Gray, byte> smooth_gray_image = My_Image.Convert<Gray, byte>();
+                Image<Gray, byte> ed_gray_image = new Image<Gray, byte>(gray_image.Size);
+                Image<Bgr, byte> final_image = new Image<Bgr, byte>(Openfile.FileName);
+                MemStorage stor = new MemStorage();
+                List<MCvBox2D> detectedLicensePlateRegionList = new List<MCvBox2D>();
+
+
+                
+
+                CvInvoke.cvEqualizeHist(gray_image, eh_gray_image);
+                CvInvoke.cvSmooth(eh_gray_image, smooth_gray_image, Emgu.CV.CvEnum.SMOOTH_TYPE.CV_GAUSSIAN, 3, 3, 0, 0);
+                //CvInvoke.cvAdaptiveThreshold(smooth_gray_image, bi_gray_image, 255, Emgu.CV.CvEnum.ADAPTIVE_THRESHOLD_TYPE.CV_ADAPTIVE_THRESH_GAUSSIAN_C, Emgu.CV.CvEnum.THRESH.CV_THRESH_BINARY, 71, 1);
+                CvInvoke.cvCanny(smooth_gray_image, ed_gray_image, 100, 50, 3);
+                Contour<Point> contours = ed_gray_image.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_TREE, stor);
+                DetectPlate(contours, detectedLicensePlateRegionList);
+
+                for (int i = 0; i < detectedLicensePlateRegionList.Count; i++)
+                {
+                    final_image.Draw(detectedLicensePlateRegionList[i], new Bgr(Color.Red), 2);
+                }
+                imageBox1.Image = My_Image;
+                imageBox2.Image = gray_image;
+                imageBox3.Image = eh_gray_image;
+                imageBox4.Image = smooth_gray_image;
+                imageBox5.Image = ed_gray_image;
+                imageBox6.Image = final_image;
+            }
+        }
+
+
+
+
+        private static int GetNumberOfChildren(Contour<Point> contours)
+        {
+            Contour<Point> child = contours.VNext;
+            if (child == null) return 0;
+            int count = 0;
+            while (child != null)
+            {
+                count++;
+                child = child.HNext;
+            }
+            return count;
+        }
+
+
+        private void DetectPlate(Contour<Point> contours, List<MCvBox2D> detectedLicensePlateRegionList)
+        {
+            for (; contours != null; contours = contours.HNext)
+            {
+                int numberOfChildren = GetNumberOfChildren(contours);
+                if (numberOfChildren == 0) continue;
+
+                if (contours.Area > 400)
+                {
+                    if (numberOfChildren < 3)
+                    {
+                        DetectPlate(contours.VNext, detectedLicensePlateRegionList);
+                        continue;
+                    }
+
+                    MCvBox2D box = contours.GetMinAreaRect();
+                    if (box.angle < -45.0)
+                    {
+                        float tmp = box.size.Width;
+                        box.size.Width = box.size.Height;
+                        box.size.Height = tmp;
+                        box.angle += 90.0f;
+                    }
+                    else if (box.angle > 45.0)
+                    {
+                        float tmp = box.size.Width;
+                        box.size.Width = box.size.Height;
+                        box.size.Height = tmp;
+                        box.angle -= 90.0f;
+                    }
+
+                    double whRatio = (double)box.size.Width / box.size.Height;
+                    if (!(3.0 < whRatio && whRatio < 10.0))
+                    {
+                        Contour<Point> child = contours.VNext;
+                        if (child != null)
+                            DetectPlate(child, detectedLicensePlateRegionList);
+                        continue;
+                    }
+                    detectedLicensePlateRegionList.Add(box);
+                }
+            }
+        }
+
+
+    }
+}
